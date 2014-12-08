dfcce2b97330ea94d6523e3a679a78105724f8ed
Francesco <zerbinati.francesco@gmail.com>
2014-12-08 18:35:00 +0100

    added images to tab bar

---
+++ SeguiTreno/TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarController.h
+//  SeguiTreno
+//
+//  Created by Francesco Zerbinati on 08/12/14.
+//  Copyright (c) 2014 Francesco Zerbinati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UITabBarController
+
+@end
---
+++ SeguiTreno/TabBarController.m
@@ -0,0 +1,45 @@
+//
+//  TabBarController.m
+//  SeguiTreno
+//
+//  Created by Francesco Zerbinati on 08/12/14.
+//  Copyright (c) 2014 Francesco Zerbinati. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+@interface TabBarController ()
+
+@end
+
+@implementation TabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [[UITabBar appearance] setTintColor:[UIColor colorWithRed:111.0/255.0 green:194.0/255.0 blue:59.0/255.0 alpha:1.0]];
+    
+    [[self.tabBar.items objectAtIndex:0] setFinishedSelectedImage:[UIImage imageNamed:@"calendarioTab"] withFinishedUnselectedImage:[UIImage imageNamed:@"calendarioTab"]];
+    [[self.tabBar.items objectAtIndex:1] setFinishedSelectedImage:[UIImage imageNamed:@"trenoTab"] withFinishedUnselectedImage:[UIImage imageNamed:@"trenoTab"]];
+    [[self.tabBar.items objectAtIndex:2] setFinishedSelectedImage:[UIImage imageNamed:@"scioperoTab"] withFinishedUnselectedImage:[UIImage imageNamed:@"scioperoTab"]];
+    [[self.tabBar.items objectAtIndex:3] setFinishedSelectedImage:[UIImage imageNamed:@"stazioniTab"] withFinishedUnselectedImage:[UIImage imageNamed:@"stazioniTab"]];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
